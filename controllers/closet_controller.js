9fe4106fe58ae4df4b86260d2486c659c65d7307
HelloBetafish <li_knee@yahoo.com>
2017-11-05 18:33:29 -0700

    added html routes to closet_controller.js

--- controllers/closet_controller.js
+++ controllers/closet_controller.js
@@ -9,20 +9,50 @@ var closet = require("../models/closet.js");
 // Create all our routes and set up logic within those routes where required.
 
 // Need to show all 
-router.get("/api/closet", function(req, res) {
+
+router.get("/login", function(req, res) {
+    res.render("login");
+   // refers to .handlebars file that will be inserted into main.handlebars.
+  });
+
+
+router.get("/:userID", function(req, res) {
+    res.render("index");
+   // refers to .handlebars file that will be inserted into main.handlebars.   // closet refers to closet.handlebars file that will cycle through mySQL and display images
+  });
+
+router.get("/createOutfit:userID", function(req, res) {
+    res.render("index_CreateOutfit");
+   // refers to .handlebars file that will be inserted into main.handlebars.   // closet refers to closet.handlebars file that will cycle through mySQL and display images
+  });
+
+router.get("/closet:userID", function(req, res) {
+    res.render("index_Closet");
+  // refers to .handlebars file that will be inserted into main.handlebars.  // closet refers to closet.handlebars file that will cycle through mySQL and display images
+  });
+
+router.get("/outfits/:userID", function(req, res) {
+    res.render("index_Outfits");
+   // refers to .handlebars file that will be inserted into main.handlebars.   // closet refers to closet.handlebars file that will cycle through mySQL and display images
+  });
+
+
+router.get("/api/closet/:userID", function(req, res) {
+  var condition = "userID = " + req.params.userID;
+  // var clothestype = req.body.clothestype;
   closet.displayType(function(data) {
-  //   var hbsObject = {
-  //     clothes: data
+    var hbsObject = {
+      clothes: data
   // clothes refers to var name in closet.handlebars
-  //   };
-  //   console.log(hbsObject);
-  //   res.render("closet", hbsObject);
-  //  closet refers to closet.handlebars file that will cycle through mySQL and display images
-  // });
+    };
+    console.log(hbsObject);
+    res.render("index_Closet", hbsObject);
+   // closet refers to closet.handlebars file that will cycle through mySQL and display images
+  });
 });
 
 router.get("/api/outfits/:userID", function(req, res) {
-  var condition = "id = " + req.params.id;
+  var condition = "userID = " + req.params.userID;
   closet.displayOutfits(condition, function(data) {
     var hbsObject = {
       outfits: data
@@ -32,32 +62,32 @@ router.get("/api/outfits/:userID", function(req, res) {
     res.render("outfits", hbsObject);
     // outfits refers to outfits.handlebars file that will cycle through mySQL and display images
   });
-  });
 });
 
-router.post("/api/closet", function(req, res) {
+
+router.post("/api/closet/:userID", function(req, res) {
   closet.uploadClothes([
     "imagepath", "clothestype", "userID"
   ], [
-    (imagepath), req.body.clothestype, req.body.userID
+    (imagepath), req.body.clothestype, req.params.userID
   ], function(result) {
     // Send back the ID of the new burger
     res.json({ id: result.insertId });
   });
 });
 
-router.post("/api/outfits", function(req, res) {
+router.post("/api/outfits/:userID", function(req, res) {
   closet.uploadOutfit([
     "outfitspath", "userID"
   ], [
-    (outfitpath), req.body.userID
+    (outfitpath), req.params.userID
   ], function(result) {
     // Send back the ID of the new burger
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/closet/:clothesID", function(req, res) {
+router.put("/api/closet/:userID", function(req, res) {
   var id = req.body.clothesID;
 
   closet.favClothes(id, function(result) {
@@ -70,7 +100,7 @@ router.put("/api/closet/:clothesID", function(req, res) {
   });
 });
 
-router.put("/api/outfits/:outfitID", function(req, res) {
+router.put("/api/outfits/:userID", function(req, res) {
   var id = req.body.outfitID;
 
   closet.favOutfit(id, function(result) {
@@ -83,7 +113,7 @@ router.put("/api/outfits/:outfitID", function(req, res) {
   });
 });
 
-router.delete("/api/closet/:clothesID", function(req, res) {
+router.delete("/api/closet/:userID", function(req, res) {
   var condition = "id = " + req.body.clothesID;
 
   closet.deleteClothes(condition, function(result) {
@@ -96,8 +126,8 @@ router.delete("/api/closet/:clothesID", function(req, res) {
   });
 });
 
-router.delete("/api/outfit/:outfitID", function(req, res) {
-  var condition = "id = " + req.body.outfitID;
+router.delete("/api/outfit/:userID", function(req, res) {
+  var condition = "userID = " + req.body.outfitID;
 
   closet.deleteOutfit(condition, function(result) {
     if (result.affectedRows == 0) {
